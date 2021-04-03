8bcf909d8a1d72f73a01a4bf2769aff6749ff49b
ZARIF ubuntu <uzbzarif@gmail.com>
2021-04-03 20:37:37 +0900

    draw gride

--- main.py
+++ main.py
@@ -2,28 +2,50 @@ import svgwrite
 
 def gride_2d(data):
     # row
-    w = len(data)
+    row = len(data)
     # column
-    h = len(data[0])
-    
+    column = len(data[0])
+    scale = 50
+    row*=scale
+    column*=scale
+    rect_size = (scale-scale*0.1,scale-scale*0.1)
+
+    dwg = svgwrite.Drawing('test.svg', profile='full',size=(column,row))
+    # Rectangle group
+    rect_g = dwg.add(dwg.g(id='rect'))
+    txt_g = dwg.add(dwg.g(id='txt'))
+    for y,row in enumerate(data):
+        print(row)
+        for x,column in enumerate(row):
+            rect=dwg.rect(
+                insert=(x*scale,y*scale),
+                size=rect_size,
+                fill='#e63946',
+                # rx=10,
+                stroke='black',
+                # stroke_width=5
+                )
+            txt = dwg.text(
+                f'{column}', 
+                insert=(x*scale+scale//2,y*scale+scale//2),
+                font_size=10, 
+                fill='#1d3557'
+                )
+            rect_g.add(rect)
+            txt_g.add(txt)
+    dwg.save()
+    return True
+
+
+
+
+
 
 array_2d = [
-    [1,2],
-    [3,4]
+    ['ID',0,1,2,3],
+    [0,7,5,3,8],
+    [1,7,5,3,8]
+    # [3,4],
 ]
-dwg = svgwrite.Drawing('test.svg', profile='full',size=(300,300))
-x = 0
-y = 10
-for i in array_2d:
-    x+=80
-    rect = dwg.rect(
-        insert=(x,y),
-        size=(50,50),
-        fill='red',
-        rx=10,
-        stroke='black',
-        stroke_width=5
-        )
-    dwg.add(rect)
 
-dwg.save()
+gride_2d(array_2d)
